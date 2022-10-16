552f0b858a8c62c6f979b6c01e27a3274f974a61
hongjie94 <hong.zhang94@gmail.com>
2022-10-16 00:57:03 -0400

    responsiveDesign

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import {useState, useEffect, useRef} from 'react';
-import './App.css';
-import Map from './Map';
-import axios from 'axios';
-import seacgIcon from './images/search.svg';
+import {useState, useEffect, useRef} from "react";
+import "./App.css";
+import Map from "./Map";
+import axios from "axios";
+import seachIcon from "./images/search.svg";
+import menuIcon from "./images/menu.svg";
 
 function App() {
   // useState
@@ -11,11 +12,12 @@ function App() {
   const [url, setUrl] = useState(null);
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [isNavExpanded, setIsNavExpanded] = useState(false)
+
 
   // useRef
-  const zipCodeRef  = useRef('');
-  const jobTitleRef = useRef('');
-
+  const zipCodeRef  = useRef("");
+  const jobTitleRef = useRef("");
 
   // submit inputs pass to url 
   const handleSearch = () => {
@@ -25,20 +27,16 @@ function App() {
     zipCodeRef.current.value = null;
     jobTitleRef.current.value = null;
 
-    const headUrl = 'https://api.adzuna.com/v1/api/jobs/us/search/10';
+    // colse nav when submit button clicked
+    setIsNavExpanded(false);
+    
+    const headUrl = "https://api.adzuna.com/v1/api/jobs/us/search/10";
     setUrl(`${headUrl}?app_id=${process.env.REACT_APP_ADZUNA_APP_ID}&app_key=${process.env.REACT_APP_ADZUNA_APP_KEY}&what=${jobTitle}&where=${zipcode}`)
   };
 
-  const getSearchResult = () =>{
-      console.log('SearchResult', data , data.length);
-  };
-
-
   // fetch datas 
   useEffect(() => { 
-    axios.get(`https://api.adzuna.com/v1/api/jobs/us/search/50?app_id=20b29dfe&app_key=25d1198733160a904c3aa1f9953ca387&what=php&where=10002
-    `)
-    // axios.get(url)
+    axios.get(url)
     .then(res => {
         setData(res.data.results)
         console.log(res.data.results)
@@ -49,30 +47,43 @@ function App() {
   }, [url]);
 
   return (
-    <div className='app'>
-      <div className="app-loading-spinner" />
+    <>
       <nav>
-        <img src={seacgIcon} alt='search icon'></img>
-        <span>
-          Job Search
-        </span>
-        <form onSubmit={e => { e.preventDefault(); handleSearch(); }}>
-        <input
-          required 
-          ref={jobTitleRef}
-          onChange = {(e)=> setJobTitle(e.target.value)} 
-          type="text" 
-          placeholder='Job title, keywords, or comany'
-        />
-        
-        <input  
-        required
-          ref={zipCodeRef}
-          onChange = {(e)=> setZipCode(e.target.value)} 
-          type="number"
-          placeholder='Zip code'>
-        </input>
-        <button className='submitBtn' disabled={isLoading} >Search</button>
+        <div className= {isNavExpanded ? "nav-header showBorder" : "nav-header"}>
+          <div class="nav-header-contents">
+            <img src={seachIcon} alt="search icon"/>
+            <span>
+              Job Search
+            </span>
+          </div>
+          <img  
+              onClick={() => {
+                setIsNavExpanded(!isNavExpanded)
+              }} 
+            className="menuIcon" 
+            src={menuIcon} alt="menu icon"
+          />
+        </div>
+        <form 
+          className= {
+            isNavExpanded ? "form show" : "form hide"
+          }
+          onSubmit={e => { e.preventDefault(); handleSearch(); }}>
+          <input
+            required 
+            ref={jobTitleRef}
+            onChange = {(e)=> setJobTitle(e.target.value)} 
+            type="text" 
+            placeholder="Job title, keywords, or comany"
+          />
+          <input  
+          required
+            ref={zipCodeRef}
+            onChange = {(e)=> setZipCode(e.target.value)} 
+            type="number"
+            placeholder="Zip code">
+          </input>
+          <button className="submitBtn" disabled={isLoading} >Search</button>
         </form>
       </nav>
       {isLoading &&  
@@ -84,8 +95,8 @@ function App() {
         </div>
         </div>
       }
-      <Map data={data}/>
-    </div>
+      <Map data={data} setIsNavExpanded={setIsNavExpanded}/>
+    </>
   );
 }
 
--- src/Map.js
+++ src/Map.js
@@ -1,108 +1,109 @@
-import {useState} from 'react';
-import MapBox, { Marker, Popup }  from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import Moment from 'react-moment';
-import pinIcon from './images/pin.png';
-import companyIcon from './images/building.png';
-import locationIcon from './images/placeholder.png'
+import {useState} from "react";
+import MapBox, { Marker, Popup }  from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import Moment from "react-moment";
+import pinIcon from "./images/pin.png";
+import companyIcon from "./images/building.png";
+import locationIcon from "./images/placeholder.png";
+import noResultImage from "./images/noResult.svg";
 
-const Map = ({data}) => {
-    const [hoveredJob, setHoveredJob] = useState(null);  
+const Map = ({data, setIsNavExpanded}) => {
 
-    // set delfault location to New York 
-    const [viewState, setViewState] = useState({
-            longitude: -73.9516030004786,
-            latitude: 40.72557300071668,
-            zoom: 10
-    });
+  const [hoveredJob, setHoveredJob] = useState(null);  
 
-    // {data &&
-    //   data.length === 0 &&
-    //    console.log('noresult')}
+  // Set delfault location to New York 
+  const [viewState, setViewState] = useState({
+    longitude: -73.9516030004786,
+    latitude: 40.72557300071668,
+    zoom: 10
+  });
 
   return (
-    <MapBox
-        {...viewState}
-        onMove={e => setViewState(e.viewState)}
-        style={{width: '100vw', height: '100vh'}}
-        mapStyle="mapbox://styles/mapbox/dark-v10"
-        mapboxAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN} >
-
-        {data && data.map(job => (
-          <Marker
-            key={job.id}
-            latitude={job.latitude}
-            longitude={job.longitude}
-            zoom = {20}
-          >
-            <button
-              className="marker-btn"
-              onMouseEnter={e => {
-                e.preventDefault();
-                console.log(job);
-                console.log(job.title);
-                setHoveredJob(job);
+    <>
+      <div class="loading-layer">
+        <div className="app-loading-spinner" />
+      </div>
+      <MapBox
+          {...viewState}
+          onMove={e => setViewState(e.viewState)}
+          style={{width: "100vw", height: "100vh"}}
+          mapStyle="mapbox://styles/mapbox/dark-v10"
+          mapboxAccessToken = {process.env.REACT_APP_MAPBOX_TOKEN} >
+          {data && data.map(job => (
+            <Marker
+              key={job.id}
+              latitude={job.latitude}
+              longitude={job.longitude}
+              zoom = {20}
+            >
+              <button
+                className="marker-btn"
+                onMouseEnter={e => {
+                  setIsNavExpanded(false);
+                  setHoveredJob(job);
+                }}
+              >
+                <img className="pin" src={pinIcon} alt="map pin" />
+              </button>
+            </Marker>
+          ))}
+          {/* If no search result */}
+          { data &&
+            data.length === 0 &&
+            <div className="noSearhResult">
+            <div className="noSearhResult-contents">
+              <div class="responsiveImgWrap"><img src={noResultImage} alt="no result found"/></div>
+              <h1>Sorry, no jobs found for that search.</h1>
+                <p>Search Suggestions: Check your keyword and location above</p>
+            </div>
+          </div>}
+          {/* Job details (pop up when map pin hovered)  */}
+          { hoveredJob ? (
+            <Popup
+              latitude={hoveredJob.latitude}
+              longitude={hoveredJob.longitude}
+              onClose={() => {
+                setHoveredJob(null);
               }}
             >
-              <img className='pin' src={pinIcon} alt="map pin" />
-            </button>
-          </Marker>
-        ))}
-
-         {/* if no search result */}
-        <div className='noSearhResult'>
-          <div className='noSearhResult-text'>
-            <img />
-            <h1>Sorry, no jobs found for that search.</h1>
-              <p>Search Suggestions: Check your keyword and location above</p>
-          </div>
-        </div>
-
-        {/* job details pop up when pin hovered */}
-        { hoveredJob ? (
-          <Popup
-            latitude={hoveredJob.latitude}
-            longitude={hoveredJob.longitude}
-            onClose={() => {
-              setHoveredJob(null);
-            }}
-          >
-          <div className='popUp'>
-            <h2>{hoveredJob.title}</h2>
-
-            <h4>
-              <img className='companyIcon' src={companyIcon} alt="company icon" /> 
-              {hoveredJob.company.display_name}
-            </h4>
-
-            <strong>
-              <img className='locationIcon' src={locationIcon} alt="location icon" /> 
-              {hoveredJob.location.display_name}
-            </strong>
-            <div className='location-detail'>
-              {(hoveredJob.location.area).map((area, index) =>(
-                  index === ((hoveredJob.location.area.length) -1) ?
-                  <span key={index}>{area}</span>:
-                  <span key={index}>{area} , </span>
-              ))}
-            </div>    
-            <div className='JobDetailInfo'> 
-              <span className="jobLabel">
-                {hoveredJob.category.label} 
-              </span>
-              <p>
-                Posted: <Moment format="MM/DD/YYYY">{hoveredJob.created}</Moment> 
-              </p>
-               <span>Job id:  {hoveredJob.id}</span>
+            <div className="popUp">
+            <a href={hoveredJob.redirect_url}> </a>
+              <h2>{hoveredJob.title}</h2>
+              <h4>
+                <img className="companyIcon" src={companyIcon} alt="company icon" />
+                {hoveredJob.company.display_name}
+              </h4>
+              <strong>
+                <img className="locationIcon" src={locationIcon} alt="location icon" />
+                {hoveredJob.location.display_name}
+              </strong>
+              <div className="location-detail">
+                {(hoveredJob.location.area).map((area, index) =>(
+                    index === ((hoveredJob.location.area.length) -1) ?
+                    <span key={index}>{area}</span>:
+                    <span key={index}>{area} , </span>
+                ))}
+              </div>
+              <div className="JobDetailInfo">
+                <span className="jobLabel">
+                  {hoveredJob.category.label}
+                </span>
+                <p>
+                  Posted: <Moment format="MM/DD/YYYY">{hoveredJob.created}</Moment>
+                </p>
+                 <span>Job id: {hoveredJob.id}</span>
+              </div>
+              <p>{hoveredJob.description}</p>
+              <div className="linkWrap">
+                <a className="redirectLink" href={hoveredJob.redirect_url} rel="noreferrer" target="_blank">
+                  Apply
+                </a>
+              </div>
             </div>
-            <p>{hoveredJob.description}</p>
-
-            <a className="redirectLink" href={hoveredJob.redirect_url} rel="noreferrer" target="_blank">Learn more</a>
-            
-          </div>
-        </Popup>
-        ) : null}   
-    </MapBox>
+          </Popup>
+          ) : null}
+      </MapBox>
+    </>
   )
 }
 
